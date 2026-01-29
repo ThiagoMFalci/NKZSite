9dacb566423d27dc2458970d28427703c24c245e
ShoraiNoPureiya <bycaferprohd2@gmail.com>
2026-01-29 12:55:58 -0300

    a

--- NKZAPI/NKZAPI/obj/Debug/net10.0/NKZAPI.AssemblyInfo.cs
+++ NKZAPI/NKZAPI/obj/Debug/net10.0/NKZAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NKZAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a8474e6a73b461e9a79fb42745638adaa4a31af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1486fb92c60eb0b395d002a53d173e0c1e022a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("NKZAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NKZAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
