aaab5704a021ba87f9da256f57e2a333313852ae
4NUB1Ss2 <soprasalvarf@gmail.com>
2026-03-22 01:47:30 -0300

    feat: creating the frontend project

---
+++ NKZFront/src/App.jsx
@@ -0,0 +1,21 @@
+import {Routes, Route, Router} from "react-router-dom";
+import Index from './Pages/Index'
+import NotFound from './Pages/NotFound'
+import Navbar from './Components/Navbar'
+
+function App() {
+    return (
+
+        <>
+            <Navbar />
+
+            <Routes>
+                <Route path="/"  element={<Index/>}/>
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
+        </>
+    )
+
+}
+
+export default App;
---
+++ NKZFront/src/Pages/NotFound/index.jsx
@@ -0,0 +1,11 @@
+function Index() {
+
+    return (
+        <>
+
+        </>
+    )
+
+}
+
+export default Index
