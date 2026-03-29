ebb21524ebd9ac1e316247c2d82391576e63517a
4NUB1Ss2 <soprasalvarf@gmail.com>
2026-03-28 21:18:27 -0300

    feat(frontend): creating the footer of the page

--- NKZFront/src/App.jsx
+++ NKZFront/src/App.jsx
@@ -1,18 +1,23 @@
-import {Routes, Route, Router} from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 import Index from './Pages/Index'
 import NotFound from './Pages/NotFound'
 import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
 
 function App() {
     return (
 
         <>
-            <Navbar />
 
-            <Routes>
-                <Route path="/"  element={<Index/>}/>
-                <Route path="*" element={<NotFound/>}/>
-            </Routes>
+                <Navbar />
+
+                <Routes>
+                    <Route path="/"  element={<Index/>}/>
+                    <Route path="*" element={<NotFound/>}/>
+                </Routes>
+
+                <Footer />
+
 
         </>
     )
